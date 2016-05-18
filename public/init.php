7272274a80325bf9ac9903235ece3e7d60798a93
Allen Herrera <aherre4@emory.edu>
2016-05-17 23:01:33 -0400

    removed startup echos from init.php

--- public/init.php
+++ public/init.php
@@ -5,9 +5,9 @@ function my_autoloader($class) {
   if(file_exists(ENGINE. $class . '.php')) {
 
     include ENGINE . $class . '.php';
-    echo "<br>including : $class";
+    //echo "<br>including : $class";
   } else {
-   echo "<br>Startup: " .ENGINE ."$class.php not found <br>";
+   //echo "<br>Startup: " .ENGINE ."$class.php not found <br>";
   }
 }
 
